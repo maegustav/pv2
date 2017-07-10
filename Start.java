a5cd78bd611f6ce507c9d52058d12e7d7e2944f4
Fabian Wurmbach <fabian.wurmbach@gmx.de>
2017-07-10 17:59:56 +0200

    asfasf

--- Start.java
+++ Start.java
@@ -17,8 +17,8 @@ class Start {
 			System.out.println("Aufruf: Start n seed");
 			System.exit(1);
 		}
-		final int n = 50;//Integer.parseInt(args[0]);
-		long seed = 444;//Long.parseLong(args[1]);
+		final int n = 80;//Integer.parseInt(args[0]);
+		long seed = 123;//Long.parseLong(args[1]);
 		final int p = places().size();
 		final int t = 6; //Integer.parseInt(System.getProperty(Configuration.APGAS_THREADS));
 
@@ -143,55 +143,58 @@ class Start {
 		long end1 = System.currentTimeMillis();
 		System.out.println(end1-start1 + "ms");
 
-/*
-
-				//berechnung sum
-				long sumValues[][] = new long[((n*n)+n)/2][((n*n)+n)/2];
+		//berechnung sum
+		at(globalFromToSum.home(), () -> {
+			long sumValues[][] = new long[((n*n)+n)/2][((n*n)+n)/2];
+			finish(() -> {
 				for(int i = 0; i < ((n*n)+n)/2; i++) {
-					y = 0;
-					for(int from = 0; from < n; from++) {
-						for(int to = from; to < n; to++) {
-							int sum2[] = new int[10];
-							for(int j = from; j <= to; j++) {
-								for(int k = 0; k < 10; k++) {
-									sum2[k] += sum[i][j][k];
+					final int fi = i;
+					async(() -> {
+						int y = 0;
+						for(int from = 0; from < n; from++) {
+							for(int to = from; to < n; to++) {
+								int sum2[] = new int[10];
+								for(int j = from; j <= to; j++) {
+									for(int k = 0; k < 10; k++) {
+										sum2[k] += globalFromToSum.get()[fi][j][k];
+									}
 								}
-							}
-							long value = 0;
-							for(int k = 0; k < 5; k++) {
-								long a = sum2[k];
-								if(sum2[k+5] > 0) {
-									a = 0;
+								for(int k = 0; k < 5; k++) {
+									long a = sum2[k];
+									if(sum2[k+5] > 0) {
+										a = 0;
+									}
+									sumValues[fi][y] += (a*( (a+1)*((2*a)+1) ) )/6;
 								}
-
-								value += (a*( (a+1)*((2*a)+1) ) )/6;
+								y++;
 							}
-							sumValues[i][y] = value;
-							y++;
 						}
-					}
+					});
 				}
+			});
 
-				long max = 0;
-				int i1 = 0;
-				int j1 = 0;
-				for(int i = 0; i < ((n*n)+n)/2; i++) {
-					for(int j = 0; j < ((n*n)+n)/2; j++) {
-						if(values[i][j] > max) {
-							max = values[i][j];
+			long max = 0;
+			int i1 = 0;
+			int j1 = 0;
+			for(int i = 0; i < ((n*n)+n)/2; i++) {
+				for(int j = 0; j < ((n*n)+n)/2; j++) {
+					if(sumValues[i][j] > max) {
+						synchronized(sumValues) {
+							max = sumValues[i][j];
 							i1 = i;
 							j1 = j;
 						}
 					}
 				}
-				int[] a = coordinates(i1, j1, n);
-				System.out.println("i1 = " + a[0] + "\tj1 = " + a[2] + "\ti2 = " + a[1] + "\tj2 = " + a[3]);
-				System.out.println("Wert: " + max);
+			}
+			int[] a = coordinates(i1, j1, n);
 
-				long end = System.currentTimeMillis();
-				System.out.println(end-start + "ms");
-		 */
+			System.out.println("i1 = " + a[0] + "\tj1 = " + a[2] + "\ti2 = " + a[1] + "\tj2 = " + a[3]);
+			System.out.println("Wert: " + max);
+		});
 
+		long end = System.currentTimeMillis();
+		System.out.println(end-start + "ms");
 
 	}
 
